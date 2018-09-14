61b4b3aebeaa2e0327aa0584499b5fb21e24c226
Tongch <fise-t@qq.com>
2018-09-14 11:23:30 +0800

    初始化项目

---
+++ src/main/java/com/tong/school_chat/SchoolChatApplication.java
@@ -0,0 +1,12 @@
+package com.tong.school_chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolChatApplication.class, args);
+    }
+}

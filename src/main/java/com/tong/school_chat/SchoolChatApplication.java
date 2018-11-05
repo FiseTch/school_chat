a845d0462937e72d30b4dabcc8f2615c24033cda
Tongch <fise-t@qq.com>
2018-11-05 14:00:24 +0800

    添加joda时期类型

--- src/main/java/com/tong/school_chat/SchoolChatApplication.java
+++ src/main/java/com/tong/school_chat/SchoolChatApplication.java
@@ -2,6 +2,9 @@ package com.tong.school_chat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class SchoolChatApplication {
@@ -9,4 +12,5 @@ public class SchoolChatApplication {
     public static void main(String[] args) {
         SpringApplication.run(SchoolChatApplication.class, args);
     }
+    
 }
